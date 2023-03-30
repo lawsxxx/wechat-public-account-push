4ef5259bcd38beccdf5bf290a4d7fdae58ad3b6e
lawsxxx <90881453+lawsxxx@users.noreply.github.com>
2023-03-30 11:19:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,9 +23,9 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '索索',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU21378TPmnzptC5HeuRqZAtq0LqEPzWzRfqXnJ2',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
