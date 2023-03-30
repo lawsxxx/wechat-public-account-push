e1301e99a6dcf9786b1e8f1bb3c9fd8ba56c3134
lawsxxx <90881453+lawsxxx@users.noreply.github.com>
2023-03-30 18:21:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ USE_PASSAGE: 'wechat-test',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU21378TPmnzptC5HeuRqZAtq0LqEPzWzRfqXnJ2',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'vMzA6wV3U8T_bZEahJsYhSV1z-UgrgD4jXtVAJDCc9U',
+      useTemplateId: '_cG6w8ZCvOgz4koes95jAF-6cRTXr5j_qmwxgI-Qxrk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-14',
       festivals: [
