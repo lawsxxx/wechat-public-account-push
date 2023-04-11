85c0afe8ee748276bb9007c58196161ed43895eb
lawsxxx <90881453+lawsxxx@users.noreply.github.com>
2023-04-11 17:51:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,8 +6,6 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-  // 请将以下代码按照图示粘贴到对应位置
-USE_PASSAGE: 'wechat-test',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx99dbe9338706f4fe',
@@ -21,26 +19,32 @@ USE_PASSAGE: 'wechat-test',
   USERS: [
     {
       // 想要发送的人的名字
-      name: '索索',
+      name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'owadk6i_uwh_yEN0ny5-JbifA7_s',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '_cG6w8ZCvOgz4koes95jAF-6cRTXr5j_qmwxgI-Qxrk',
+      useTemplateId: 'b5njzpOihH8a1Jkj2v-TiW-XdjPeP53Jn_Fhcblhezc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '09-14',
+      horoscopeDate: '12-27',
       festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '索索', year: '1995', date: '09-14',
+          type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2023', date: '02-05',
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-03-18' },
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
@@ -53,7 +57,7 @@ USE_PASSAGE: 'wechat-test',
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21378TPmnzptC5HeuRqZAtq0LqEPzWzRfqXnJ2',
+      id: '',
     }
   ],
 
